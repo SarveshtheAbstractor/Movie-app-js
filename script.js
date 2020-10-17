c12e6ead28328197b660b09309d36a571c48330b
Sarvesh Hiwase <69983214+SarveshtheAbstractor@users.noreply.github.com>
2020-10-17 12:30:43 +0530

    Update script.js
    adding randomness to pages

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const pagerandom = Math.floor(Math.random()*500);
 const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+    `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=${pagerandom}`;
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
